b24be2a1846edbd2bf39d84db51aa4076e5b1c0f
MrOdie <sodonnell5214@gmail.com>
2022-07-23 14:30:47 -0400

    Changing Project from NodeJS based to Java

---
+++ src/test/java/com/example/castirondev/CastirondevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.castirondev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastirondevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
